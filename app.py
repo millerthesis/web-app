6da7ce558a89fcd1a8e4f5d6bfba2e19cb961348
Dan Nguyen <dansonguyen@gmail.com>
2018-05-11 10:58:45 -0700

    styles

--- app.py
+++ app.py
@@ -14,18 +14,15 @@ def state(statecode):
     rawhtml = render_template('state.html', state=get_state(statecode), US=get_us())
     return rawhtml
 
-@myapp.route("/sample")
-def examples():
-    return render_template('sample.html')
 
-@myapp.route("/methodology")
-def methodology():
-    return render_template('methodology.html')
-   
+@myapp.route("/info/<endpoint>")
+def infopage(endpoint):
+    return render_template('infopages/{}.html'.format(endpoint))
 
-@myapp.route("/testdata")
-def xfoo():
-    return render_template('altairtest.html')
+
+# @myapp.route("/testdata")
+# def xfoo():
+#     return render_template('altairtest.html')
 
 if __name__ == '__main__':
     myapp.run(debug=True, use_reloader=True)
